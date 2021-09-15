d78aa2f3c7e63813e106eac547cf0d086b264c29
宁 双 <972126586@qq.com>
2021-09-15 11:52:56 +0800

    Initial commit

---
+++ lesson5_常量/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lession5_常量
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("常量");
+        }
+    }
+}
---
+++ lesson6_转义字符/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson_转义字符
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
