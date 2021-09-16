859c8c20044db4f89ab0c848f6b2ba758740781a
宁 双 <972126586@qq.com>
2021-09-16 18:34:50 +0800

    再次提交

--- lesson5_常量/Program.cs
+++ lesson5_常量/Program.cs
@@ -7,6 +7,13 @@ namespace lession5_常量
         static void Main(string[] args)
         {
             Console.WriteLine("常量");
+            //关键字 const
+            //固定写法：const 变量类型 变量名 = 初始值
+            //变量的申明
+            int i = 10;
+            //常量的申明
+            const int m = 20;
+
         }
     }
 }
