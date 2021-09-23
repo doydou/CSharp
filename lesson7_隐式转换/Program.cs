6283544bbc4305cb00bcac3586c685702fec93f2
宁 双 <972126586@qq.com>
2021-09-23 16:47:39 +0800

    补充

---
+++ lesson7_隐式转换/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("隐式转换");
+            //类型转换——不同类型之间的相互转换
+            //隐式转换的基本规则--不同类型之间的自动转换
+            //大范围转小范围
+            
+        }
+    }
+}
