8b1f39a6d4d8c9f53d935f3bf3ea68f23ae43701
宁 双 <972126586@qq.com>
2022-10-12 11:19:34 +0800

    文件结构调整

---
+++ lesson8_异常捕获/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson8_异常捕获
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
