1cbafd4a241370cf406eed513b0d1b98915a6008
宁 双 <972126586@qq.com>
2022-10-31 17:44:19 +0800

    基础完结

--- lesson10_条件语句/Program.cs
+++ lesson10_条件语句/Program.cs
@@ -6,15 +6,38 @@ namespace lesson12_条件运算符
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("条件运算符");
-            //条件运算符 一定存在左右两边的内容
-
-            #region 知识点一 条件运算符
+            Console.WriteLine("条件语句");
             
+
+            #region 知识点一 if语句
+            //if（bool类型值）-（语句块）-else—（语句块）
+            //if语句后不用分号且可以嵌套
             
 
             #endregion
-            
+
+            #region 知识点二 switch语句
+            //switch只判断变量是否等于一个变量值
+            //switch--case --break(无数个case)--（default_当上面的case的条件都不满足时）--break
+            int a = 3;
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("a=1");
+                    break;                
+                case 3:
+                    Console.WriteLine("a=3");
+                    break;
+                default:
+                    Console.WriteLine("都不对");
+                    break;
+
+            }
+            //default可忽略
+            //可自定义常量
+            //贯穿 满足某些条件时，不用写case后面的break。
+
+            #endregion
         }
     }
 }
---
+++ lesson11_循环语句/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson11_循环语句
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
