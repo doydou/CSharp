4bd9c5a8348597c2d0152ad547d194bcf961f3d5
宁 双 <972126586@qq.com>
2022-11-01 18:15:25 +0800

    补充提交

--- lesson11_循环语句/Program.cs
+++ lesson11_循环语句/Program.cs
@@ -6,7 +6,27 @@ namespace lesson11_循环语句
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("循环语句");
+            //让代码可以被重复执行
+
+            #region 知识点一 语法相关
+            //打印1-20的奇数
+            int i = 1;
+            while (i < 20)
+            {
+                Console.WriteLine(i);
+                i = i + 2;  
+            }
+
+            #endregion
+
+            #region 知识点二
+
+            
+
+
+
+            #endregion
         }
     }
 }
