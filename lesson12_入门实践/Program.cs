e45fffe8dbb9ab0eafa8b2a551debe9c8fc4a691
宁 双 <972126586@qq.com>
2023-03-22 17:36:20 +0800

    入门实践

---
+++ lesson12_入门实践/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson12_入门实践
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
